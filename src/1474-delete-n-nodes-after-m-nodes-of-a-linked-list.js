8d88f49b90b27cd52c5202b2031a7d8600aab20c
aaronlam <aaronlam1618@gmail.com>
2021-03-26 20:53:25 -0700

    feat(1474): added solution 1474

---
+++ src/1474-delete-n-nodes-after-m-nodes-of-a-linked-list.js
@@ -0,0 +1,23 @@
+const moveTo = (node, steps) => {
+  for (let i = 0; i < steps && node != null; i++) {
+    node = node.next;
+  }
+  return node;
+}
+
+const deleteNodes = (head, m, n) => {
+  if (head == null || m <= 0 || n <= 0) {
+    return null;
+  }
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let cur = dummy;
+  while (cur != null) {
+    cur = moveTo(cur, m);
+    if (cur == null) {
+      return head;
+    }
+    cur.next = moveTo(cur.next, n);
+  }
+  return dummy.next;
+};
