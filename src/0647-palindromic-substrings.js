77cd4b9667f36c31266e0e2df027187e2ce6ffb4
aaronlam <aaronlam1618@gmail.com>
2021-03-27 11:04:21 -0700

    feat(647): added solution 647

---
+++ src/0647-palindromic-substrings.js
@@ -0,0 +1,21 @@
+const countSubstrings = (s) => {
+  if (s == null || s.length === 0) {
+    return 0;
+  }
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    res += extendPalindrome(s, i, i);
+    res += extendPalindrome(s, i, i + 1);
+  }
+  return res;
+};
+
+const extendPalindrome = (s, start, end) => {
+  let length = 0;
+  while (start >= 0 && end < s.length && s.charAt(start) === s.charAt(end)) {
+    start -= 1;
+    end += 1;
+    length += 1;
+  }
+  return length;
+}
