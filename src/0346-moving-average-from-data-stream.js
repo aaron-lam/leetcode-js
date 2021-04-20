3895fff4435614bd8f283bb5be6dadb485537bd5
aaronlam <aaronlam1618@gmail.com>
2021-04-20 12:17:50 -0700

    feat(0346): added solution 346

---
+++ src/0346-moving-average-from-data-stream.js
@@ -0,0 +1,16 @@
+class MovingAverage {
+  constructor(size) {
+    this.q = [];
+    this.sum = 0;
+    this.size = size;
+  }
+  
+  next(val) {
+    if (this.q.length === this.size) {
+      this.sum -= this.q.shift();
+    }
+    this.q.push(val);
+    this.sum += val;
+    return this.sum / this.q.length;
+  }
+}
