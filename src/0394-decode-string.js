efd71c4a2f57e14f02440b47f11c91de2bd5e7d3
aaronlam <aaronlam1618@gmail.com>
2021-04-23 19:36:03 -0700

    fix(394): updated solution 394

---
+++ src/0394-decode-string.js
@@ -0,0 +1,25 @@
+const isNum = (c) => !isNaN(c);
+
+const decodeString = (s) => {
+  if (s == null || s.length === 0) {
+    return "";
+  }
+  const stack = [];
+  let curNum = 0;
+  let curStr = "";
+  for (c of s.split("")) {
+    if (c === '[') {
+      stack.push([curStr, curNum]);
+      curStr = "";
+      curNum = 0;
+    } else if (c == "]") {
+      const [prevStr, num] = stack.pop();
+      curStr = prevStr + curStr.repeat(num);
+    } else if (isNum(c)) {
+      curNum = curNum * 10 + Number(c);
+    } else {
+      curStr += c;
+    }
+  }
+  return curStr;
+}
