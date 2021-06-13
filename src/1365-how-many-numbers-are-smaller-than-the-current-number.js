d712aa7cd44850d64c4fccd36503f6f233b0f501
aaronlam <aaronlam1618@gmail.com>
2021-06-12 22:17:10 -0700

    fix: 1365

---
+++ src/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,5 @@
+const smallerNumbersThanCurrent = (nums) => {
+  const sortedNums = Array.from(nums).sort((a, b) => b - a);
+  const map = new Map(sortedNums.map((num, index) => [num, nums.length - index - 1]));
+  return nums.map(num => map.get(num));
+};
